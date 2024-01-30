f5044ebc32396bd2cdca15ca53661d147f96493b
SumanSaha <8759127658itisaha@gmail.com>
2024-01-30 20:01:49 +0530

    beginning

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* <--- Left Side ---> */}
+
+  <div className="leftSide">
+
+  </div>
+
+  {/* <--- Right Side ---> */}
+  
+    <div className="rightSide">
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
