fa70a55f3b691044b1a630ac70d5fff9f995c220
SumanSaha <8759127658itisaha@gmail.com>
2024-01-30 21:04:30 +0530

    Day-1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,11 +7,13 @@ function App() {
       {/* <--- Left Side ---> */}
 
   <div className="leftSide">
-
+<div className="logo">
+  <img src="https://static.vecteezy.com/system/resources/previews/022/227/364/non_2x/openai-chatgpt-logo-icon-free-png.png" alt="" /><span>ChatGPT</span>
+</div>
   </div>
 
   {/* <--- Right Side ---> */}
-  
+
     <div className="rightSide">
 
     </div>
