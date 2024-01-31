d8696056c9ca2dba9fc74d35f9503a9932234767
SumanSaha <8759127658itisaha@gmail.com>
2024-01-31 16:34:33 +0530

    Add responsive menu icon and left side mid section.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import menuBarIcon from "./menu.png"
 
 function App() {
   return (
@@ -6,9 +7,16 @@ function App() {
 
       {/* <--- Left Side ---> */}
 
+<div className="resMenuIcon">
+  <img src="https://cdn1.iconfinder.com/data/icons/basic-ui-elements-color-round/3/01-512.png" alt="" />
+</div>
+
   <div className="leftSide">
 <div className="logo">
   <img src="https://static.vecteezy.com/system/resources/previews/022/227/364/non_2x/openai-chatgpt-logo-icon-free-png.png" alt="" /><span>ChatGPT</span>
+</div>
+<div className="leftmid">
+    <button><img src="https://www.freepnglogos.com/uploads/plus-icon/plus-icon-plus-math-icon-download-icons-9.png" alt="" /></button>
 </div>
   </div>
 
