bfdbf4e8e21f2eab97d8dd748bc18f85c4708f67
SumanSaha <8759127658itisaha@gmail.com>
2024-02-01 15:49:07 +0530

    added openAI api

--- my-app/src/assets/openAi.jsx
+++ my-app/src/assets/openAi.jsx
@@ -1,1 +1,5 @@
-key = sk-"OYpPORxormMarHMrcCmBT3BlbkFJrQAl8xrfAtr26jwlCgM5"
+// key = "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"
+import { Configuration, openaAIApi } from "openai";
+
+const configuration = new Configuration ({apikey :"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" });
+const openai = new openaAIApi(configuration)
